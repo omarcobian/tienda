502d4c489c0a58cc5d3f0df62e746c2a733b50a7
Tu Nombre <marvinteck63@gmail.com>
2025-09-24 21:28:39 -0600

    feat: add dashboard, api routes and simplify layout
    - Add Dashboard page with General component
    - Create admin and sucursal API routes
    - Remove unused favicon and simplify globals.css
    - Clean up layout and home page to basic structure

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,9 @@
+import General from "@/components/general/general"
+
+export default function page() {
+  return (
+    <div className="text-4xl text-cyan-600">datos generales
+     <General/>
+    </div>
+  )
+}
---
+++ src/components/general/general.tsx
@@ -0,0 +1,12 @@
+export default function General() {
+  return (
+    <div>
+        <h1>datos generales</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptas.
+        </p>
+
+    </div>
+  )
+}
